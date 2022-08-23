8489b1ad98e43d6a6bb8bd97762f7e0dbfa38ab1
Akashic Bearer <kurihararei1@gmail.com>
2022-08-23 16:50:16 +0000

    GLobal / Command Support * Needs bot to be re-invited with new permissions! * /Info command so people know where commands went

--- src/DokiDoki.js
+++ src/DokiDoki.js
@@ -1,11 +1,14 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+
 const fs = require('node:fs');
 const path = require('node:path');
 const dotenv = require('dotenv');
 dotenv.config();
 const chalk = require('chalk');
-//	const src = require('./Require.js');
 const configs = require('./configs/client.json');
+const clientId = configs.clientId
 
 const Token = process.env['BOT_TOKEN'];
 //	const DevToken = process.env['DEV_BOT_TOKEN'];
@@ -17,12 +20,11 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 	if (configs.DevStatus === 2) {
-		console.log('𝕃𝕠𝕒𝕕𝕖𝕕 ℂ𝕠𝕞𝕞𝕒𝕟𝕕: ' + chalk.green(command.data.name));
+		console.log('Loaded Commands: ' + chalk.green(command.data.name));
 	}
 }
 
@@ -36,6 +38,9 @@ for (const file of eventFiles) {
 	}
 	else {
 		client.on(event.name, (...args) => event.execute(...args));
+	}
+	if (configs.DevStatus === 2) {
+		console.log('Loaded Events: ' + chalk.green(event.name));
 	}
 }
 
@@ -52,6 +57,29 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+client.on('guildCreate', async guild => {
+	let guildId = guild.id;
+
+	const commands = [];
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		commands.push(command.data.toJSON());
+		if (configs.DevStatus === 2) {
+			console.log(command.data);
+		}
+	}
+
+	const rest = new REST({ version: '10' }).setToken(Token);
+
+	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+		.then(() => console.log('Successfully registered application commands.'))
+		.catch(console.error);
+})
 
 if (configs.DevStatus === 2) {
 	client.login(Token);
